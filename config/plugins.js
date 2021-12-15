d046fe58c9b6e88460d16b10b411a26cb24cff87
O Gustavo <avoguga@gmail.com>
2021-12-15 04:11:53 -0300

    graphql ativado

--- config/plugins.js
+++ config/plugins.js
@@ -17,5 +17,17 @@ module.exports = ({ env }) => ({
       },
     },
   },
+
+  graphql: {
+    endpoint: '/graphql',
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 100,
+    apolloServer: {
+      tracing: false,
+    },
+  }
+  
   // ...
 });
