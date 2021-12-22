8948b08f777b9b3d91c024dd77ff15f818741819
O Gustavo <avoguga@gmail.com>
2021-12-21 23:45:38 -0300

    bla

--- config/plugins.js
+++ config/plugins.js
@@ -17,17 +17,5 @@ module.exports = ({ env }) => ({
       },
     },
   },
-
-  graphql: {
-    endpoint: '/graphql',
-    shadowCRUD: true,
-    playgroundAlways: false,
-    depthLimit: 7,
-    amountLimit: 100,
-    apolloServer: {
-      tracing: false,
-    },
-  }
-  
   // ...
 });
